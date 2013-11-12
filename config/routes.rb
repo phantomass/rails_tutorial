502c80d3235d2d718cc794bf3eb93a0f817ba7a7
phantomass <ilnurgilfanov7@gmail.com>
2013-11-12 20:39:11 +0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SampleApp::Application.routes.draw do
   resources :users
-  root            to: 'static_pages#home'
-  get '/signup',  to: 'users#new'
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root              to: 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
